a77d4161001c479ec2e1f8ac35b8dcad724f16c1
KpaPijnenburg <kpapijnenburg@gmail.com>
2019-03-28 19:00:19 +0100

    Initial commit

--- src/main/java/api/Server.java
+++ src/main/java/api/Server.java
@@ -2,9 +2,10 @@ package api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class Server {
+public class Server extends SpringBootServletInitializer {
     public static void main(String[] args) {
         SpringApplication.run(Server.class);
     }
